0eb6d18d7702d38d06ce876fc4852fc4748cd9cc
Tuan Tran <tranvantuanueh@gmail.com>
2022-04-13 00:40:26 +0700

    LAB02_2 done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
+import { Navbar, NavbarBrand } from "reactstrap";
 import './App.css';
 class App extends Component {
 
   render() {
     return (
       <div className="App">
-        <p>Sample cho toàn bộ lab và project trong môn học Lập Trình Web Front-End với React với đầy đủ dependency.</p>
-        <p>Phiên bản React hiện tại trên máy của bạn là: {React.version}</p>
-        <p>Phiên bản React đã được sử dụng để thiết kế môn học: 16.14.0.</p>
+        <Navbar dark color="primary">
+          <div className='container'>
+            <NavbarBrand href="/">Brand name here</NavbarBrand>
+          </div>
+        </Navbar>
+        
+
       </div>
     );
   }

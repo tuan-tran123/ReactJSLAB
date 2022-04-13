a82feb4174cd89d8defa2d1c4b7618c52dc1bdcd
Tuan Tran <tranvantuanueh@gmail.com>
2022-04-13 12:40:08 +0700

    LAB03_1 DONE

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Navbar, NavbarBrand } from "reactstrap";
+import Menu from './components/MenuComponent';
 import './App.css';
 class App extends Component {
 
@@ -11,7 +12,7 @@ class App extends Component {
             <NavbarBrand href="/">Brand name here</NavbarBrand>
           </div>
         </Navbar>
-        
+        <Menu />
 
       </div>
     );

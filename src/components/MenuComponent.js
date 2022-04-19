aade9d8c7e6b6c16d73ef597872228e5ef6acb81
Tuan Tran <tranvantuanueh@gmail.com>
2022-04-19 16:32:29 +0700

    SPA 2

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,17 +1,19 @@
 import React from 'react';
-import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
-
+import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
     
-   function RenderMenuItem({dish, onClick}) {
+   function RenderMenuItem({dish}) {
        return (
-         <Card onClick={() => onClick(dish.id) }>
-            <CardImg width="100%" src={dish.image} alt={dish.name}/> 
-                            
-            <CardImgOverlay>
-            <CardTitle>{dish.name}</CardTitle>
-
-            </CardImgOverlay>
-        </Card>
+           <Card>
+               <Link to= {`/menu/${dish.id}`}>
+                    <CardImg width="100%" src={dish.image} alt={dish.name}/>                 
+                    <CardImgOverlay>
+                    <CardTitle>{dish.name}</CardTitle>
+                    </CardImgOverlay>
+        
+               </Link>
+            </Card>
+            
        )
    }
     
@@ -21,13 +23,23 @@ import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 're
             
             return (
                 <div key={dish.id} className="col-12 col-md-5 m-1"> 
-                  <RenderMenuItem  dish={dish} onClick={props.onClick} />
+                  <RenderMenuItem  dish={dish} />
                     
                 </div>
             ) // key define every item 
         });
         return (
             <div className="container">
+                <div className='row'>
+                    <Breadcrumb>
+                        <BreadcrumbItem><Link to='/home'>Home</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>Menu</BreadcrumbItem>
+                    </Breadcrumb>
+                    <div className='col-12'>
+                        <h3>Menu</h3>
+                        <hr />
+                    </div>
+                </div>
                 <div className='row'>
 
                         {menu}

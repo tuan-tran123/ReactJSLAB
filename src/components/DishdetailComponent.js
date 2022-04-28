de21fdf028f4422770878c8ee6abd57779ca2893
Tuan Tran <tranvantuanueh@gmail.com>
2022-04-29 00:20:10 +0700

    LAB 11 DONE

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -4,19 +4,27 @@ import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors  } from 'react-redux-form';
 import { Loading } from './LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
+import { FadeTransform, Fade, Stagger } from "react-animation-components";
 
 function RenderDish({ dish }) {
 
         
         return (
                 <div className='col-12 col-md-5 m-1'>
+                 <FadeTransform in
+                    transformProps={{
+                    exitTransform: 'scale(0.5) translate(-50%'
+                    }}>
+                    
                     <Card>
-                        <CardImg width="100%" src={baseUrl + dish.image} alt={dish.name} />    
-                        <CardBody>
-                            <CardTitle>{dish.name}</CardTitle>
-                            <CardText>{dish.description}</CardText>
-                        </CardBody>
-                    </Card>
+                            <CardImg width="100%" src={baseUrl + dish.image} alt={dish.name} />    
+                            <CardBody>
+                                <CardTitle>{dish.name}</CardTitle>
+                                <CardText>{dish.description}</CardText>
+                            </CardBody>
+                    </Card>                    
+                </FadeTransform>   
+                
                 </div>
                 
                 
@@ -33,15 +41,21 @@ function RenderComments({ comments, addComment, dishId }) {
                  <div className='col-12 col-md-5 m-1'>
                      
                      <h4>Comments</h4>                     
-                     <ul className='list-unstyled'>                         
-                         {comments.map((comment) => {
-                             return (
-                                 <li key={comment.id}>
-                                     <p>{comment.comment}</p>
-                                     <p>--{comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(comment.date)))} </p>
-                                 </li>
-                             )
-                         })}
+                     <ul className='list-unstyled'>       
+                        <Stagger in>
+                            {comments.map((comment) => {
+                                return (
+                                    <Fade in>                                    
+                                        <li key={comment.id}>
+                                            <p>{comment.comment}</p>
+                                            <p>--{comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(comment.date)))} </p>
+                                            </li>
+                                    </Fade>
+                                    
+                                )
+                            })}
+                         </Stagger>   
+                         
                      </ul>
                      
                     <CommentForm dishId={dishId} addComment={addComment} />    
--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,7 +1,8 @@
 import React from "react";
-import { Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle } from "reactstrap";
+import { Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle, Fade } from "reactstrap";
 import { Loading } from "./LoadingComponent";
 import { baseUrl } from "../shared/baseUrl";
+import { FadeTransform } from "react-animation-components";
 
 function RenderCard({ item, isLoading, errMess }) {
     if (isLoading) {
@@ -14,15 +15,21 @@ function RenderCard({ item, isLoading, errMess }) {
         );
     } else
         return (
-            <Card>
-                <CardImg src={baseUrl+ item.image} alt={item.name} />
-                <CardBody>
-                    <CardTitle>{item.name}</CardTitle>
-                    {item.designation ? <CardSubtitle>{item.designation}</CardSubtitle> : null}
-                    {/*Nếu item designation khác null, trả về CardSubtitle ngươc lại : trả về null*/}
-                    <CardText>{item.description}</CardText>
-                </CardBody>
-            </Card>
+            <FadeTransform in
+            transformProps={{
+                exitTransform: 'scale(0.5) translate(-50%'
+            }}>
+                <Card>
+                    <CardImg src={baseUrl+ item.image} alt={item.name} />
+                    <CardBody>
+                        <CardTitle>{item.name}</CardTitle>
+                        {item.designation ? <CardSubtitle>{item.designation}</CardSubtitle> : null}
+                        {/*Nếu item designation khác null, trả về CardSubtitle ngươc lại : trả về null*/}
+                        <CardText>{item.description}</CardText>
+                    </CardBody>
+                    </Card>
+            </FadeTransform>
+            
         );
 }
 
